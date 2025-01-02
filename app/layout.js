2f999c8ac4de8b9a5f09dd6ecdb50ad501dd0f08
Faraazrehan2002 <faraazrehan2002@gmail.com>
2025-01-02 21:52:03 +0530

    Authentication Done

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -14,21 +15,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${font.className}`}>
-        {/* header */}
-        <Header/>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${font.className}`}>
+          {/* header */}
+          <Header/>
 
-        <main className="min-h-screen">{children}</main>
-        {/* footer */}
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>
-              Made by Faraaz Rehan Junaidi Mohammed
-            </p>
-          </div>
-        </footer>
-      </body>
-    </html>
+          <main className="min-h-screen">{children}</main>
+          {/* footer */}
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>
+                Made by Faraaz Rehan Junaidi Mohammed
+              </p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
