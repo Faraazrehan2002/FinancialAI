d583e4b9acc6a173e92cb2f9b176112a0068735e
Faraazrehan2002 <faraazrehan2002@gmail.com>
2025-01-31 20:24:42 -0600

    Features Section

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,8 @@
 import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { statsData } from "./data/landing";
+import { featuresData, statsData } from "./data/landing";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function Home() {
   return (
@@ -21,6 +22,25 @@ export default function Home() {
                 </div>
               </div>
              ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-12 ">
+            Everything you need to manage your finances
+          </h2>
+          <div className="grid grid-cols-2 md:grid-cols-2 lg:gird-cols-3 gap-8 ">
+            {featuresData.map((feature, index)=>(
+              <Card key = {index} className = "p-6 ">
+                <CardContent className="space-y-4 pt-4 ">
+                  {feature.icon}
+                  <h3 className="text-xl font-semibold ">{feature.title}</h3>
+                  <p className="text-gray-600">{feature.description}</p>
+                </CardContent>
+              </Card>
+            ))}
           </div>
         </div>
       </section>
