a4398292c9fa702b4d090c4d1c6ef822676889bd
Mirza Abdul Aleem Baig <mirza@Mirzas-MacBook-Air.local>
2025-02-03 07:55:32 -0600

    Database Setup and middleware

--- components/header.jsx
+++ components/header.jsx
@@ -2,12 +2,14 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 // import { Link } from 'next/link'
+import { checkUser } from '@/lib/checkUser'
 
 import React from 'react'
 import { Button } from './ui/button'
 import { LayoutDashboard, PenBox } from 'lucide-react'
 
-const Header = () => {
+const Header = async() => {
+    await checkUser()
   return (
     <div className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
         <nav className='container mx-auto px-4 py-4 flex items-center justify-between'>
